52fd949dbf64431375048a98e52c2deddb25179e
Joy Wang <joywang0222@gmail.com>
2025-12-27 15:07:06 -0800

    gmail client finished

--- gmail_client.py
+++ gmail_client.py
@@ -1,28 +1,118 @@
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import base64
+import html
+import os
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 class GmailClient:
-    def __init__(self):
-        pass
+    def __init__(self, credentials_path: str="credentials.json", token_path: str="token.json"):
+        """
+        Initialize Gmail Client with OAuth Credentials
+        
+        :param self: Description
+        :param credentials_path: path to oauth credentials json
+        :type credentials_path: str
+        :param token_path: path to save/load auth token
+        :type token_path: str
+
+        """
+        self.credentials_path = credentials_path
+        self.token_path = token_path
+        creds = None
+        if os.path.exists(token_path):
+            creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentials_path, SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            with open (token_path, 'w') as file:
+                file.write(creds.to_json())
+
+        self.service = build("gmail", "v1", credentials=creds)
+        
 
     def list_messages(self, max_results: int = 10) -> list:
         """
         Returns a list of recent emails, limited by max_results
         
-        Args:
-            max_results (int) : The maximum number of emails returned, with a default of 10
+        :param self: Description
+        :param max_results: maximum number of emails returned
+        :type max_results: int
+        :return: a list of max_results emails
+        :rtype: list
         """
-        pass
+        results = self.service.users().messages().list(
+            userId="me",
+            maxResults=max_results
+        ).execute()
+        return results.get("messages", [])
+        
     def get_message(self, message_id: str) -> dict:
         """
         Returns the details of a specific email by its id
-
-        Args:
-            message_id (str): The unique identifier of the email
+        
+        :param self: Description
+        :param message_id: The unique identifier of the email
+        :type message_id: str
+        :return: Full message data
+        :rtype: dict
+        
         """
-        pass
-    def parse_message(self, dict: str) -> dict:
+        return self.service.users().messages().get(
+            userId="me",
+            id=message_id,
+            format='full'
+        ).execute()
+        
+    def parse_message(self, message: dict) -> dict:
         """
         Parses raw message from Gmail API into a structured format
-        Args:
-            dict (str): The raw message data from Gmail API
+        
+        :param self: Description
+        :param dict: The raw message data from Gmail API
+        :type dict: str
+        :return: Parsed message with subject, from, to, date, body
+        :rtype: dict
+        
         """
-        pass
+        headers = message["payload"]["headers"]
+        parsed = {
+            'id': message['id'],
+            'threadId': message['threadId'],
+            'snippet': html.unescape(message.get('snippet', ''))
+        }
+        keys = ["subject", "from", "to", "date"]
+        for header in headers:
+            name = header["name"].lower()
+            if name in keys:
+                parsed[name] = header["value"]
+        parsed["body"] = self._get_body(message['payload'])
+
+        return parsed
+        
+    def _get_body(self, payload: dict) -> str:
+        if 'body' in payload and 'data' in payload['body']:
+            return self._decode_body(payload['body']['data'])
+        if 'parts' in payload:
+            for part in payload['parts']:
+                if part.get('mimeType') == 'text/plain':
+                    if 'data' in part.get('body', {}):
+                        return self._decode_body(part['body']['data'])
+            for part in payload['parts']:
+                if part.get('mimeType') == 'text/html':
+                    if 'data' in part.get('body', {}):
+                        return self._decode_body(part['body']['data'])
+        return 'Could not extract body'
+
+    def _decode_body(self, body: str) -> str:
+        decoded_bytes = base64.b64decode(body)
+        text = decoded_bytes.decode('utf-8')
+        return text.replace('\r\n', '\n').replace('\r', '\n')
---
+++ test_gmail.py
@@ -0,0 +1,18 @@
+from gmail_client import GmailClient
+
+client = GmailClient()
+
+messages = client.list_messages()
+
+# for message in messages:
+#     print(f"id:{message['id']} thread_id:{message['threadId']}")
+first = messages[1]
+msg_id = first['id']
+full_msg = client.get_message(msg_id)
+parsed = client.parse_message(full_msg)
+print(f"From: {parsed['from']}")
+print(f"Subject: {parsed['subject']}")
+print(f"Date: {parsed['date']}")
+print(f"\nBody:\n{parsed['body']}")
+
+# print(f"Snippet: {full_msg.get('payload')}")
